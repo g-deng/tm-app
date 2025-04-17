1ccc794cce0de2784c32fe14b3c7b402fe6e7bb6
GraceD15 <graceful.deng@gmail.com>
2025-04-17 14:30:35 -0400

    Recovered stashed files that were deleted in upstream

---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import { StateData, TransitionData } from './elems';
+
+export interface SingleTransitionAction {
+    action: 'delete' | 'create' | 'edit';
+    type: 'transition';
+    item: TransitionData;
+}
+
+export interface SingleStateAction {
+    action: 'delete' | 'create' | 'edit';
+    type: 'state';
+    item: StateData;
+}
+
+export interface MultipleAction {
+    action: 'create-multiple' | 'delete-multiple';
+    state: StateData;
+    transitions: TransitionData[];
+}
+
+export type Point = { x: number; y: number };
+
+export type UserAction = SingleTransitionAction | SingleStateAction | MultipleAction;
